52a188774e3b0575c369bcf96a84e0bd52d620fe
Hannah Coffman <hcoffman7@gmail.com>
2019-03-25 22:30:25 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,14 @@
+// problem is we want to react to clicks on the <main> tag 
+
+const main = document.getElementById('main')
+
+// AddEventListener has 2 options 
+// 1. is the event that we care about 
+// 2. is the function that is supposed to run when that event happens 
+
+function onClick() {
+  console.log('hey the click happened')
+}
+
+main.AddEventListener('click',onClick)
+
